4f1d6349472c642d78a37ed87a0acc28e591fef2
Diomedes Diaz <d.romeldiaz@gmail.com>
2025-06-29 16:35:34 -0500

    initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,46 @@
+export const languageList = {
+    es: 'Español',
+    en: 'English',
+}
+
+export const labels = {
+    es: {
+        'quote.button': 'Cotiza',
+        'home.primaryTitle': 'Alquiler y Venta de Maquinaria de Alto Rendimiento para Proyectos Exigentes',
+        'home.secondaryTitle': 'Soluciones de maquinaria confiable con servicio técnico especializado y disponibilidad inmediata.',
+        'clients.title': 'Clientes satisfechos',
+        'clients.client1Description': 'Aumenta la credibilidad de los productos y servicios al incluir testimonios de los clientes. Las recomendaciones son muy apreciadas, por lo que los comentarios de otras personas que ya los han probado tienen un valor incalculable.',
+        'clients.client2Description': 'Aumenta la credibilidad de los productos y servicios al incluir testimonios. <br />Las recomendaciones son muy apreciadas, por lo que los comentarios de otras personas que ya los han probado tienen un valor incalculable.',
+        'clients.client3Description': 'Aumenta la credibilidad de los productos y servicios al incluir testimonios de los clientes. Las recomendaciones son muy apreciadas, por lo que los comentarios de otras personas que ya los han probado tienen un valor incalculable. <br /><br />¡Inclúyelas aquí!',
+        'clients.client1Name': 'Acústica',
+        'clients.client2Name': 'María y asociados',
+        'clients.client3Name': 'Estudio Rayo',
+
+        'contact.title': 'Formulario De Contacto',
+        'footer.workWithUs': 'Trabaja con nosotros',
+        'footer.address': 'Dirección',
+        'footer.WorkingHours': 'Horario de atención',
+        'footer.socialMedia': 'Redes sociales',
+        'footer.addressValue': 'Calle 123, Colonia, Ciudad, Estado C.P. 12345 <br />(estacionamiento disponible para PCD) <br /><br />Tel. (55) 1234 5678 <br />Correo: hola@sitioincreible.com',
+        'footer.WorkingHoursValue': 'De Lunes a Viernes <br />9:00 AM - 6:00 PM <br /><br />Fines de semana con cita previa',
+    },
+    en: {
+        'quote.button': 'Quote',
+        'home.primaryTitle': 'High-Performance Machinery Rental and Sales for Demanding Projects',
+        'home.secondaryTitle': 'Reliable machinery solutions with specialized technical service and immediate availability.',
+        'clients.title': 'Satisfied Clients',
+        'clients.client1Description': 'Increase the credibility of products and services by including client testimonials. Recommendations are highly valued, so feedback from others who have already tried them is invaluable.',
+        'clients.client2Description': 'Increase the credibility of products and services by including testimonials. <br />Recommendations are highly valued, so feedback from others who have already tried them is invaluable.',
+        'clients.client3Description': 'Increase the credibility of products and services by including client testimonials. Recommendations are highly valued, so feedback from others who have already tried them is invaluable. <br /><br />Include them here!',
+        'clients.client1Name': 'Acoustics',
+        'clients.client2Name': 'Maria and Associates',
+        'clients.client3Name': 'Ray Studio',
+        
+        'footer.workWithUs': 'Work with us',
+        'footer.address': 'Address',
+        'footer.WorkingHours': 'Working Hours',
+        'footer.socialMedia': 'Social Media',
+        'footer.addressValue': '123 Street, Neighborhood, City, State ZIP 12345 <br />(parking available for PWD) <br /><br />Tel. (55) 1234 5678 <br />Email: hola@sitioincreible.com',
+        'footer.WorkingHoursValue': 'Monday to Friday <br />9:00 AM - 6:00 PM <br /><br />Weekends by appointment only',
+    }
+};
